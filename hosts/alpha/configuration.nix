d3142d6185f0f2962b09e43d3252380fc957d19e
nktfh100 <me@nktfh100.com>
2023-09-27 10:52:14 +0300

    Update alpha wallpaper

--- hosts/alpha/configuration.nix
+++ hosts/alpha/configuration.nix
@@ -1,8 +1,14 @@
 { ... }:
 
 {
-  imports =
-    [ ./hardware-configuration.nix ../../common.nix ../../modules/nvidia.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ../../common.nix
+    ../../modules/nvidia.nix
+    (import ../../modules/wallpaper.nix {
+      wallpaper = "file:///etc/nixos/wallpapers/wallhaven-j3m8y5.png";
+    })
+  ];
 
   networking.hostName = "nktfh100-alpha";
 }
