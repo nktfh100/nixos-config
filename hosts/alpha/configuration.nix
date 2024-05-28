133728506bf8e86e5f99c663416c23553f3945f4
nktfh100 <me@nktfh100.com>
2024-05-28 13:50:39 +0300

    Update flake

--- hosts/alpha/configuration.nix
+++ hosts/alpha/configuration.nix
@@ -15,14 +15,4 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot =
     true; # powers up the default Bluetooth controller on boot
-
-  hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
-    load-module module-bluetooth-policy
-    load-module module-bluetooth-discover
-    ## module fails to load with
-    ##   module-bluez5-device.c: Failed to get device path from module arguments
-    ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-    # load-module module-bluez5-device
-    # load-module module-bluez5-discover
-  '';
 }
