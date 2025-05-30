55bf450e095a2a28ab919525d953aa61a6357ded
nktfh100 <me@malachi.io>
2025-05-30 23:41:29 +0300

    WIP

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,9 +1,14 @@
 { pkgs, unstable, ... }:
 
 {
-  system.activationScripts.neovimSymLink.text = ''
+  system.activationScripts.hyprlandSymLink.text = ''
     rm -f -r /home/nktfh100/.config/hypr
-    ln -Ts /etc/nixos/modules/hyperland/hyprland-config /home/nktfh100/.config/hypr
+    ln -Ts /etc/nixos/modules/hyprland/hyprland-config /home/nktfh100/.config/hypr
+  '';
+
+  system.activationScripts.makoSymLink.text = ''
+    rm -f -r /home/nktfh100/.config/mako/config
+    ln -Ts /etc/nixos/modules/hyprland/mako-config /home/nktfh100/.config/mako/config
   '';
 
   programs.hyprland.enable = true;
@@ -14,6 +19,13 @@
     kdePackages.dolphin # File manager
     wofi # Application launcher
     nwg-displays # Monitor configuration
+    waybar # Status bar
+    wl-clipboard # Clipboard
+    hyprshot # take screenshots
+    hyprpicker # Color picker
+
+    mako # Notification daemon
+    # hyprpaper # Wallpaper manager
   ];
 
   # hint Electron apps to use Wayland:
